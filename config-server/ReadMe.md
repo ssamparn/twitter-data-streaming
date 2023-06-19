5f2c3008b7fff39dc004b227d8f1c6bdefb37a74
samantrs <https://samantrs@git.rabobank.nl>
2023-06-19 18:19:27 +0200

    web security config

--- config-server/ReadMe.md
+++ config-server/ReadMe.md
@@ -1,3 +1,7 @@
 ### Go to this link to load all config properties
 
 [Click me](http://localhost:8888/config-client/twitter_to_kafka)
+
+[To Encrypt](POST http://localhost:8888/encrypt)
+
+[To Decrypt](POST http://localhost:8888/decrypt)
---
+++ config-server/src/main/java/com/spring/microservices/config/server/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.spring.microservices.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authz) -> authz
+                    .requestMatchers(
+                            "/config-client/twitter_to_kafka",
+                            "/actuator/**",
+                            "/encrypt/**",
+                            "/decrypt/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic(withDefaults());
+        return http.build();
+    }
+}
